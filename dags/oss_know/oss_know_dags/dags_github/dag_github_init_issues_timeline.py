470b8a7250a987d2898f3532fed7283c59f586e7
Zhen Ju <juzhenatpku@gmail.com>
2024-07-10 18:47:19 +0800

    Impelment the 'since' logic in timeline init DAG

--- dags/oss_know/oss_know_dags/dags_github/dag_github_init_issues_timeline.py
+++ dags/oss_know/oss_know_dags/dags_github/dag_github_init_issues_timeline.py
@@ -4,11 +4,10 @@ from airflow import DAG
 from airflow.models import Variable
 from airflow.operators.python import PythonOperator
 
-from oss_know.libs.base_dict.opensearch_index import OPENSEARCH_GIT_RAW, OPENSEARCH_INDEX_GITHUB_ISSUES_TIMELINE
+from oss_know.libs.base_dict.opensearch_index import OPENSEARCH_INDEX_GITHUB_ISSUES_TIMELINE
 from oss_know.libs.base_dict.variable_key import NEED_INIT_GITHUB_ISSUES_TIMELINE_REPOS, GITHUB_TOKENS, \
     OPENSEARCH_CONN_DATA, PROXY_CONFS, CK_TABLE_DEFAULT_VAL_TPLT, CLICKHOUSE_DRIVER_INFO
-from oss_know.libs.clickhouse import init_ck_transfer_data
-from oss_know.libs.github import init_issues_timeline
+from oss_know.libs.util.data_transfer import sync_clickhouse_from_opensearch
 from oss_know.libs.util.proxy import GithubTokenProxyAccommodator, ProxyServiceProvider, \
     make_accommodator
 
@@ -29,29 +28,20 @@ with DAG(
                                            GithubTokenProxyAccommodator.POLICY_FIXED_MAP)
 
 
-    def do_init_github_issues_timeline(params):
-        owner = params["owner"]
-        repo = params["repo"]
-        # since = params["since"]
-        since = None
+    def do_init_github_issues_timeline(owner, repo, since=None):
         init_issues_timeline.init_sync_github_issues_timeline(opensearch_conn_info, owner, repo,
                                                               proxy_accommodator, since)
 
-        return params
 
-
-    def do_ck_transfer(owner, repo):
-        search_key = {"owner": owner, "repo": repo}
+    def do_ck_transfer(owner, repo, since=None):
         table_templates = Variable.get(CK_TABLE_DEFAULT_VAL_TPLT, deserialize_json=True)
-
         template = table_templates.get(OPENSEARCH_INDEX_GITHUB_ISSUES_TIMELINE)
-        init_ck_transfer_data.transfer_data_by_repo(
-            clickhouse_server_info=clickhouse_server_info,
-            opensearch_index=OPENSEARCH_INDEX_GITHUB_ISSUES_TIMELINE,
-            table_name=OPENSEARCH_INDEX_GITHUB_ISSUES_TIMELINE,
-            opensearch_conn_datas=opensearch_conn_info,
-            template=template, owner_repo_or_project_maillist_name=search_key,
-            transfer_type='github_git_init_by_repo')
+
+        sync_clickhouse_from_opensearch(owner, repo,
+                                        OPENSEARCH_INDEX_GITHUB_ISSUES_TIMELINE, opensearch_conn_info,
+                                        OPENSEARCH_INDEX_GITHUB_ISSUES_TIMELINE, clickhouse_server_info,
+                                        template,
+                                        since)
 
 
     need_do_init_ops = []
@@ -64,10 +54,20 @@ with DAG(
     for timeline_owner_repo in github_issues_timeline_repos:
         owner = timeline_owner_repo["owner"]
         repo = timeline_owner_repo["repo"]
+        since = timeline_owner_repo.get('since')
+
+        if since:
+            # Validate since before execution
+            datetime.strptime(since, '%Y-%m-%dT%H:%M:%SZ')
+
         op_do_init_github_issues_timeline = PythonOperator(
             task_id=f'do_init_github_issues_timeline_{owner}_{repo}',
             python_callable=do_init_github_issues_timeline,
-            op_kwargs={'params': timeline_owner_repo},
+            op_kwargs={
+                'owner': owner,
+                'repo': repo,
+                'since': since
+            },
         )
 
         op_transfer_data_to_ck = PythonOperator(
@@ -76,6 +76,7 @@ with DAG(
             op_kwargs={
                 'owner': owner,
                 'repo': repo,
+                'since': since
             },
         )
 
